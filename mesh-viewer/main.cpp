9c0ae5ec5f5517976660d84f6ef0ae2d78877ed7
stegogo <lizmahrk@gmail.com>
2022-09-12 17:23:47 +0300

    add-logo

--- mesh-viewer/main.cpp
+++ mesh-viewer/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 {
     QGuiApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
     QApplication a(argc, argv);
-
+    a.setWindowIcon(QIcon(":/img/meshlogo.ico"));
 
     AppSkeleton mw;
 
